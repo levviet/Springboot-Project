5915f1a5f5f5925580efc073c10d82f6a6c0fd74
vietlvhe180475 <vietlvhe180475@fpt.edu.vn>
2025-04-10 02:59:12 +0700

    Fix Mapstruct

--- src/main/java/com/example/learn_spring_boot/service/UserService.java
+++ src/main/java/com/example/learn_spring_boot/service/UserService.java
@@ -5,6 +5,7 @@ import com.example.learn_spring_boot.dto.request.UpdateUserRequest;
 import com.example.learn_spring_boot.entity.User;
 import com.example.learn_spring_boot.exception.AppException;
 import com.example.learn_spring_boot.exception.ErrorCode;
+import com.example.learn_spring_boot.mapper.UserMapper;
 import com.example.learn_spring_boot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,18 +17,17 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private UserMapper userMapper;
+
 	public User create(CreateUserRequest request) {
-		User user = new User();
 
 		if (userRepository.existsUserByUsername(request.getUsername())) {
 			throw new AppException(ErrorCode.USER_EXISTED);
 		}
 
-		user.setUsername(request.getUsername());
-		user.setPassword(request.getPassword());
-		user.setFirstName(request.getFirstName());
-		user.setLastName(request.getLastName());
-		user.setDob(request.getDob());
+		User user = userMapper.toUser(request);
+
 		return userRepository.save(user);
 	}
 
