09d8345b04136c3940cca1aafcc2b37dc4d4e7f0
vietlvhe180475 <vietlvhe180475@fpt.edu.vn>
2025-06-08 00:52:08 +0700

    Return code and message when user unauthenticated and authorized

--- src/main/java/com/example/learn_spring_boot/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/learn_spring_boot/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.learn_spring_boot.exception;
 
 import com.example.learn_spring_boot.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,7 +26,9 @@ public class GlobalExceptionHandler {
 		apiResponse.setCode(errorCode.getCode());
 		apiResponse.setMessage(errorCode.getMessage());
 
-		return ResponseEntity.badRequest().body(apiResponse);
+		return ResponseEntity
+				.status(errorCode.getStatusCode())
+				.body(apiResponse);
 	}
 
 	@ExceptionHandler(value = MethodArgumentNotValidException.class)
@@ -45,5 +48,17 @@ public class GlobalExceptionHandler {
 		apiResponse.setMessage(errorCode.getMessage());
 
 		return ResponseEntity.badRequest().body(apiResponse);
+	}
+
+	@ExceptionHandler(value = AccessDeniedException.class)
+	ResponseEntity<ApiResponse> handlingAccessDeniedException(AccessDeniedException e) {
+		ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+
+		return ResponseEntity
+				.status(errorCode.getStatusCode())
+				.body(ApiResponse.builder()
+						.code(errorCode.getCode())
+						.message(errorCode.getMessage())
+						.build());
 	}
 }
