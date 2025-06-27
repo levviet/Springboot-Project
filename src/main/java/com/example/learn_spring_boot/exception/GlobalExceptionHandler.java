b07f901fda7a47d21dc7b0aafb401ef00707ceb0
vietlvhe180475 <vietlvhe180475@fpt.edu.vn>
2025-06-27 23:31:58 +0700

    Get Param For Message for Validation

--- src/main/java/com/example/learn_spring_boot/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/learn_spring_boot/exception/GlobalExceptionHandler.java
@@ -1,14 +1,21 @@
 package com.example.learn_spring_boot.exception;
 
 import com.example.learn_spring_boot.dto.ApiResponse;
+import jakarta.validation.ConstraintViolation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
+import java.util.Objects;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+	private static final String MIN_ATTRIBUTE = "min";
+
 	@ExceptionHandler(value = Exception.class)
 	ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException e) {
 		ApiResponse apiResponse = new ApiResponse();
@@ -36,16 +43,24 @@ public class GlobalExceptionHandler {
 		String enumKey = e.getFieldError().getDefaultMessage();
 
 		ErrorCode errorCode = ErrorCode.INVALID_KEY;
+		Map<String, Object> attributes = null;
 
 		try {
 			errorCode = ErrorCode.valueOf(enumKey);
+
+			var constrantViolation = e.getBindingResult().getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+
+			attributes = constrantViolation.getConstraintDescriptor().getAttributes();
+
 		} catch (IllegalArgumentException exception) {
 
 		}
 
 		ApiResponse apiResponse = new ApiResponse();
 		apiResponse.setCode(errorCode.getCode());
-		apiResponse.setMessage(errorCode.getMessage());
+		apiResponse.setMessage(Objects.nonNull(attributes) ?
+				mapAttribute(errorCode.getMessage(), attributes) :
+				errorCode.getMessage());
 
 		return ResponseEntity.badRequest().body(apiResponse);
 	}
@@ -60,5 +75,11 @@ public class GlobalExceptionHandler {
 						.code(errorCode.getCode())
 						.message(errorCode.getMessage())
 						.build());
+	}
+
+	private String mapAttribute(String message, Map<String, Object> attributes) {
+		String min = attributes.get(MIN_ATTRIBUTE).toString();
+
+		return message.replace("{" + MIN_ATTRIBUTE + "}", min);
 	}
 }
